e065c3ca54a6b2654dbb2e4b0b34228106557a10
amancern3 <amandeep842.as@gmail.com>
2026-01-06 21:55:29 -0800

    feature(consumer, sentinel): Restructure main function and implement ZTFSentinel and LasairConsumer in consumer package

--- cmd/sentinel/main.go
+++ cmd/sentinel/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
-	"github.com/bitterfq/ztf-sentinel/internal"
+	"github.com/bitterfq/ztf-sentinel/internal/consumer"
+	"github.com/bitterfq/ztf-sentinel/internal/database/db"
+	"github.com/bitterfq/ztf-sentinel/internal/repository"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 
-	err := godotenv.Load()
+	_ = godotenv.Load()
+	dbConn, err := sql.Open("postgres", os.Getenv("DBCONN"))
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal(err)
 	}
+	queries := db.New(dbConn)
 
-	consumer, err := internal.NewLasairConsumer(
+	kafkaConsumer, err := consumer.NewLasairConsumer(
 		os.Getenv("HOST"),
 		os.Getenv("GROUP_ID"),
 		os.Getenv("TOPIC"),
@@ -25,10 +31,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sentinel, err := internal.NewZTFSentinel(
+	sentinel, err := consumer.NewZTFSentinel(
 		"logs/event.log",
 		"logs/error.log",
-		consumer,
+		kafkaConsumer,
+		repository.NewPostgresRepo(queries),
 	)
 
 	if err != nil {
---
+++ internal/consumer/consumer.go
@@ -0,0 +1,133 @@
+package consumer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/bitterfq/ztf-sentinel/internal/domains"
+	"github.com/bitterfq/ztf-sentinel/internal/repository"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type LasairConsumer struct {
+	kafkaConsumer *kafka.Consumer
+}
+
+type ZTFSentinel struct {
+	eventLog       *os.File
+	errorLog       *os.File
+	LasairConsumer *LasairConsumer
+	repo           repository.AlertRepository
+}
+
+func NewZTFSentinel(evntLog string, errLog string, consumer *LasairConsumer, repo repository.AlertRepository) (*ZTFSentinel, error) {
+
+	sentinel := ZTFSentinel{
+		eventLog:       nil,
+		errorLog:       nil,
+		LasairConsumer: consumer,
+		repo:           repo,
+	}
+
+	// open event & err logs
+	var err error
+
+	sentinel.errorLog, err = os.OpenFile(errLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("FAILED TO OPEN ERROR LOG FILE: %w", err)
+	}
+	sentinel.eventLog, err = os.OpenFile(evntLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("FAILED TO OPEN EVENT LOG FIL: %w", err)
+	}
+
+	sentinel.eventLog.WriteString("🪐 Initializing ZTF Sentinel....\n")
+
+	return &sentinel, nil
+}
+
+func NewLasairConsumer(host, groupId, topic string) (*LasairConsumer, error) {
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": host,
+		"group.id":          groupId,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Initialize Kafka Consumer: %w", err)
+	}
+
+	err = c.Subscribe(topic, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to Subscribe to Kafka topic: %w", err)
+	}
+
+	return &LasairConsumer{
+		kafkaConsumer: c,
+	}, err
+}
+
+func (lc *LasairConsumer) Poll(timeoutMs int) kafka.Event {
+	event := lc.kafkaConsumer.Poll(timeoutMs)
+	return event
+}
+
+func (lc *LasairConsumer) Close() error {
+	return lc.kafkaConsumer.Close()
+}
+
+func (sentinel *ZTFSentinel) CloseFiles() error {
+	err := sentinel.eventLog.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = sentinel.errorLog.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func (sentinel *ZTFSentinel) Run() {
+
+	ctx := context.Background()
+	for {
+		// TODO: add flag to pass in user defined timeout args
+		msg := sentinel.LasairConsumer.Poll(3600)
+		now := time.Now().UTC().Format(time.RFC3339)
+
+		switch e := msg.(type) {
+		case kafka.Error:
+			fmt.Fprintf(sentinel.errorLog, "[%s] %s\n", now, msg.String())
+
+		case *kafka.Message:
+
+			// create domain type
+			var alert domains.Alert
+
+			// unmarshall data into domain type
+			err := json.Unmarshal(e.Value, &alert)
+			if err != nil {
+				fmt.Fprintf(sentinel.errorLog, "[%s] %s\n", now, msg.String())
+			}
+
+			// for all that is not in the data packet, populate our alert type with
+			alert.ID = fmt.Sprintf("%s_%f", alert.ObjectID, alert.LatestDetection)
+			alert.ReceivedAt = time.Now()
+
+			err = sentinel.repo.Save(ctx, alert)
+			if err != nil {
+				fmt.Fprintf(sentinel.errorLog, "[%s] %s\n", now, msg.String())
+			}
+
+		case nil:
+			continue
+		}
+
+	}
+
+}
